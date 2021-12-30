b826f5bf090a25486bd3dbda1928710db13b51d4
Kevin Castillo <kcastilloo1@est.ups.edu.ec>
2021-12-30 09:23:26 -0500

    Commit inicial W

---
+++ scratch.py
@@ -0,0 +1,10 @@
+# Script de prueba para Git
+# Autor: Kevin Castillo <kcastilloo1@est.ups.edu.ec>
+# Fecha: 30/12/2021
+# Versión: 1.0.0
+
+def main():
+    print("Hola Mundo!")
+
+if __name__ == "__main__":
+    main()
